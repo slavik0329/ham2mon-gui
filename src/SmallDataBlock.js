9d5d2e0f8229661c7ff6d07d5202d038c3d59c22
Steve Dakh <slavik0329@gmail.com>
2020-04-15 10:50:16 -0400

    screenshot

--- src/SmallDataBlock.js
+++ src/SmallDataBlock.js
@@ -5,15 +5,15 @@ export function SmallDataBlock({Icon, data}) {
   const styles = {
     container: {
       display: "inline-block",
-      marginRight: 8,
+      marginRight: 10,
       color: primary4
     },
     icon: {
       marginRight: 2,
-      width: 12,
-      height: 12,
+      width: 14,
+      height: 14,
       position:'relative',
-      top: 1
+      top: 2
     },
     data: {
       marginLeft: 2
